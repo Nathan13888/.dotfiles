b9ea5a68bfbe8a75f2d7016477e1fd7df963ceb8
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-05-17 14:04:03 -0400

    symlink commands to INSTALL script and some symlink fixing

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,4 +9,11 @@ ln -s ${BASEDIR}/vim/ ~/.vim
 # git
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 
+# vscode
+
+ln -s ${BASEDIR}/.vscode ~/workspace/Git/.vscode
+
+# fish
+ln -s ${BASEDIR}/fish ~/.config/fish
+
 ## NEED TO ADD OTHER DotFiles

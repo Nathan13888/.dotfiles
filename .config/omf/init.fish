b3420ce6dc56987bb18ee963f9255a254c4f4552
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-05-18 13:00:56 -0400

    fix symbolic links problems with omf

---
+++ .config/omf/init.fish
@@ -0,0 +1,31 @@
+# ALIAS
+
+## SHORTCUTS
+alias v="vim"
+alias reload='omf reload' # reload fish
+
+## TYPOS
+alias gti="git"
+
+## COMMANDS
+alias git 'hub'
+alias lla "ls -alF"
+alias lsa "ls -aF"
+
+## SSH
+alias sshdev 'ssh attackercow@192.168.10.69'
+alias sshnas 'ssh attackercow@192.168.10.250'
+alias sshnvr 'ssh attackercow@192.168.10.140'
+alias sshwpc 'ssh tvbox@192.168.10.121'
+
+## Navigation
+alias ..="cd .."
+alias cd..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+#alias ~="cd ~" # cd home
+#alias -- -="cd -"
+
+## Permissions
+alias chmox='chmod -x'

6e0147348e8b4682905fd93841ddac612951732f
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-06-03 13:07:32 -0400

    update aliases

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -3,41 +3,34 @@
 #########
 
 ## SHORTCUTS
+alias vim="nvim" # comment this out if you don't what to use vim
 alias sv="sudo vim"
 alias v="vim"
 alias vr='v -r'
 alias g="git"
 alias git='hub' # eval "$(hub alias -s)" # what's the difference even??
-alias l='le'
-alias le="less"
-alias m='mo'
-alias mo="more"
-alias s 'sudo'
+alias l="less"
+alias m="more"
+alias s='sudo'
 alias sudo='sudo ' # for using other alias when using sudo
-alias free="free -hm"
-alias df="df -h"
-alias du="du -h"
 alias service="sudo service"
 alias serv="service"
 alias code="code-insiders"
 alias bdctl="betterdiscordctl"
-alias shutdown="sudo shutdown now"
-alias reboot="sudo reboot now"
-alias reload="omf reload" # reload fish
 alias chmox='sudo chmod +x'
 alias chowm='sudo chown $USER:$USER'
 alias su="sudo su -"
 alias c="clear"
-alias cl="clear"
-alias cls="clear"
 alias clear="clear -x"
+alias reload="omf reload" # reload fish
+alias shutdown="sudo shutdown now"
+alias reboot="sudo reboot now"
 
 ## TYPOS
+alias f='g'
 alias gi='git'
 alias gti='git'
 alias cd..="cd .."
-alias f='g'
-# This really isn't a problem since I rarely do typos and could easily fix them if I do...
 
 ## SSH
 alias sshdev 'ssh attackercow@192.168.10.69'
@@ -46,6 +39,21 @@ alias sshnvr 'ssh attackercow@192.168.10.140'
 alias sshwpc 'ssh tvbox@192.168.10.121'
 
 ## INFO and NAVIGATION
+alias disks='df -h | grep sd \
+    | sed -e "s_/dev/sda[1-9]_\x1b[34m&\x1b[0m_" \
+    | sed -e "s_/dev/sd[b-z][1-9]_\x1b[33m&\x1b[0m_" \
+    | sed -e "s_[,0-9]*[MG]_\x1b[36m&\x1b[0m_" \
+    | sed -e "s_[0-9]*%_\x1b[32m&\x1b[0m_" \
+    | sed -e "s_9[0-9]%_\x1b[31m&\x1b[0m_" \
+    | sed -e "s_/mnt/[-_A-Za-z0-9]*_\x1b[34;1m&\x1b[0m_"'
+
+alias du="du -h"
+alias df="df -h"
+alias du1="du -d 1"
+alias free="free -hm"
+alias uptime='uptime -p'
+alias top="htop" # HTOP is better than TOP
+
 alias lla="ls -AlhF --color=always --group-directories-first" 
 alias lld="lla | grep --color=always '^d'"
 alias llf="lla | grep --color=always '^-'"
@@ -59,9 +67,7 @@ alias cp='cp -iv'
 alias mv='mv -iv'
 alias rm='rm -iv'
 
-alias du1="du -d 1"
-
-alias fdir='find . -type d -name' # find directory
+alias fd='find . -type d -name' # find directory
 alias ff='find . -type f -name' # find file
 
 alias pers "cd /mnt/nas/personal"

74a8b2e7920b9b3f57b16255e0059b487bbe76aa
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-09-03 12:28:40 -0400

    path: add .local/bin
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -2,6 +2,8 @@
 export PATH="$PATH:/usr/local/go/bin/"
 # CUDA
 export PATH="$PATH:/usr/local/cuda-11.0/bin"
+# Fetch Cord - https://github.com/MrPotatoBobx/FetchCord#installing-on-gnulinux
+export PATH="$HOME/.local/bin:$PATH"
 
 export PYTHONDONTWRITEBYTECODE=1
 

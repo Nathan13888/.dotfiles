531e1c5ab099952b6778792f6688e14436c414c9
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-11-01 12:27:46 -0500

    zsh: move exports from path.zsh
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -1,13 +1,3 @@
-# GO LANG
-export PATH="$PATH:/usr/local/go/bin/"
-# CUDA
-export PATH="$PATH:/usr/local/cuda-11.0/bin"
-# Fetch Cord - https://github.com/MrPotatoBobx/FetchCord#installing-on-gnulinux
-export PATH="$HOME/.local/bin:$PATH"
-
 export PYTHONDONTWRITEBYTECODE=1
-
-# LS_COLORS
-source ~/.local/share/lscolors.sh
 
 [ -f ~/.exports ] && source ~/.exports

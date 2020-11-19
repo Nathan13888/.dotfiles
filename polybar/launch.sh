d04907ccc33b96d9a3d81ab72dccc37c5d9a3848
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-11-19 14:41:19 -0500

    polybar: improve launch script
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,6 +7,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bar-i3 &
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  #MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+  FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload bar-i3 >> /tmp/polybar.log 2>&1 & disown
+done
 
 echo "Polybar launched..."

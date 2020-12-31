96b7a7f4e23e2c9cb57ed8f2ec8e247c740f4c9a
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-12-31 10:07:03 -0500

    fetchcord.sh: update script
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- fetchcord.sh
+++ fetchcord.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+TERMINAL=$TERMINAL
+echo "TERMINAL is $TERMINAL"
 
+LOG=/tmp/fetchcord.$(date +"%Y%m%d_%H%M%S").log
+echo "Logging to $LOG"
+
+echo "Killing all other instances of Fetchcord" | tee $LOG
 killall fetchcord
 
+echo "Sleeping for 3 seconds before starting" | tee $LOG
 sleep 3
+echo "Starting..."
 
-fetchcord --nohardware --nohost --terminal $TERMINAL --termfont "MesloLGS Nerd Font"
+fetchcord --nohardware --nohost --terminal "$TERMINAL" --termfont "MesloLGS Nerd Font" | tee $LOG
 
 

73a137e990e7a4378b7bd6936aa5008e8c7582ee
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-01-29 21:20:35 -0500

    polybar: use a different bar if specified
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,8 +9,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  #MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
-  FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload bar-i3 >> /tmp/polybar.log 2>&1 & disown
+    #MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
+    bar="bar-i3"
+    if [ -n "$1" ]; then
+        bar=$1
+    fi
+    FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload $bar >> /tmp/polybar.log 2>&1 & disown
 done
 
 echo "Polybar launched..."

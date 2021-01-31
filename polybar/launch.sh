83c45f624a1df8159822e6ec36861ca629f48a83
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-01-31 18:17:11 -0500

    polybar: update launch script
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -15,6 +15,9 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
     if [ -n "$1" ]; then
         bar=$1
     fi
+
+    echo "Launching $bar"
+
     FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload $bar >> /tmp/polybar.log 2>&1 & disown
 done
 

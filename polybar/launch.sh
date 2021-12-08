a9becb1ce75f4016fff1011d9e44928d18cb66e5
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-12-07 19:45:15 -0500

    polybar: update log file
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -18,7 +18,8 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
 
     echo "Launching $bar"
 
-    FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload $bar >> /tmp/polybar.log 2>&1 & disown
+    TMP="$(mktemp -u)_polybar"
+    FC_DEBUG=1 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload $bar > $TMP 2>&1 & disown
 done
 
 echo "Polybar launched..."

c9580a413127004097a60295de416a8c5a6bf470
Nathan <29968201+Nathan13888@users.noreply.github.com>
2026-02-13 15:23:31 -0800

    add neovim plugins: treesitter, LSP, completion, formatting, telescope, catppuccin
    Set leader to comma. Configure treesitter for 14 languages, mason +
    lspconfig for 5 language servers (using vim.lsp.config API), blink.cmp
    for completion, conform.nvim for format-on-save (biome for JS/TS),
    telescope for fuzzy finding, and catppuccin mocha colorscheme.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,1 +1,121 @@
-return {}
+return {
+  -- Treesitter: syntax highlighting & indentation
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "rust", "typescript", "tsx", "javascript", "go", "python", "lua",
+        "bash", "json", "yaml", "toml", "html", "css", "markdown",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+
+  -- Mason: auto-install language servers
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "rust_analyzer", "ts_ls", "gopls", "pyright", "lua_ls",
+      },
+    },
+  },
+
+  -- LSP config (vim.lsp.config API, nvim 0.11+)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+      })
+
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+
+      vim.lsp.enable({ "rust_analyzer", "ts_ls", "gopls", "pyright", "lua_ls" })
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    opts = {
+      keymap = { preset = "default" },
+      completion = { documentation = { auto_show = true } },
+      signature = { enabled = true },
+    },
+  },
+
+  -- Format on save
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt" },
+        typescript = { "biome" },
+        javascript = { "biome" },
+        typescriptreact = { "biome" },
+        javascriptreact = { "biome" },
+        go = { "gofmt" },
+        python = { "ruff_format" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
+
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+    },
+  },
+
+  -- Colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { flavour = "mocha" },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}

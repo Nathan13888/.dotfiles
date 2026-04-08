e02c6a2b30a7985c4dce31efd734fa09cb09aa9d
Nathan <29968201+Nathan13888@users.noreply.github.com>
2026-04-08 01:28:01 +0000

    nvim git blame
    Signed-off-by: Nathan <29968201+Nathan13888@users.noreply.github.com>

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,6 +2,11 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 200,
+      },
+      current_line_blame_formatter = "  <author>, <author_time:%R> - <abbrev_sha> - <summary>",
       on_attach = function(bufnr)
         local gs = require("gitsigns")
         local function map(mode, l, r, desc)
@@ -17,6 +22,7 @@ return {
         map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
         map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
         map("n", "<leader>hd", gs.diffthis, "Diff this")
+        map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
         -- Visual mode
         map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage selection")
         map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset selection")
